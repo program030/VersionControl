9d85ce96f044512bb9d3fa80aefb6bd1b03c0cc8
program030 <thanandon445@gmail.com>
2025-07-09 10:54:51 +0700

    Change label to Thai language

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -9,8 +9,8 @@
       <q-input
         filled
         v-model="name"
-        label="Your name *"
-        hint="Name and surname"
+        label="ชื่อ-สกุล *"
+        hint="กรุณาใส่ชื่อและนามสกุล"
         lazy-rules
         :rules="[ val => val && val.length > 0 || 'Please type something']"
       />
@@ -83,3 +83,4 @@ export default {
   }
 }
 </script>
+
